abccff3998a2216ac0523d962556a431538f974f
Daniel Jünger <juenger@uni-mainz.de>
2019-10-11 01:49:59 +0200

    more detailed example

--- example.cu
+++ example.cu
@@ -5,11 +5,19 @@ int main (int argc, char *argv[])
 {
     // payload of packed data type
     using base_t = std::uint64_t;
+
     // packed type with three fields: 13 bits, 8 bits, 34 bits, (+ 9 bits padding)
     using pack_t = PackedTriple<base_t, 13, 8, 34>;
-    // using pack_t = PackedPair<base_t, _, _>;
-    // using pack_t = PackedQuadruple<base_t, _, _, _, _>;
+    // or using pack_t = PackedPair<base_t, _, _>;
+    // or using pack_t = PackedQuadruple<base_t, _, _, _, _>;
     
+    std::cout 
+        << "size of pack_t="  
+        << sizeof(base_t)
+        << " is equal to the size of base_t="
+        << sizeof(base_t)
+        << std::endl;
+        
     // print data layout (curly braces denote padding bits)
     std::cout 
         << "bit partition: [[" 
@@ -44,7 +52,9 @@ int main (int argc, char *argv[])
         << ")" 
         << std::endl;
 
-    // and member functions with similar syntax i.e. get<>/set<>()
+    // and member functions with similar syntax i.e. get<>()/set<>()
+    // NOTE you may need to provide additional info to the compiler 
+    // e.g. "triple.template get<0>()"
     std::cout 
         << "triple.get<> triple = (" 
         << triple.get<0>() 
@@ -57,10 +67,23 @@ int main (int argc, char *argv[])
 
     // update third field
     triple.third(42);
+    // or triple.set<2>(42) or triple.template set<2>(42)
     std::cout << "third = " << triple.third() << std::endl;
 
     // following line should trigger an assertion error since 12345 needs more than 8 bit
     // triple.second(12345);
-    std::cout << "should be false: " << pack_t::is_valid_second(12345) << std::endl;
-    std::cout << "maximum value for second field is " << pack_t::second_max() << std::endl;
+    std::cout 
+        << std::boolalpha
+        << "should be false: " 
+        << pack_t::is_valid_second(12345) 
+        << std::endl;
+
+    std::cout 
+        << "maximum value for second field is " 
+        << pack_t::second_max() 
+        << std::endl;
+
+    // support for atomic updates:
+    // comes with specializations for CUDA's atomicCAS() and atomicExch()
+    // also valid: std::atomic<pack_t> 
 }
